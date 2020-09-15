8ab89a52b077cf1d037a8c4eba05a261e90adb9f
lizeyang1995 <424011527@qq.com>
2020-09-15 21:14:09 +0800

    [lizeyang] feat: write and complete a test, should modify rs event when provide key word and event name

--- src/test/java/com/thoughtworks/rslist/api/RsControllerTest.java
+++ src/test/java/com/thoughtworks/rslist/api/RsControllerTest.java
@@ -1,6 +1,9 @@
 package com.thoughtworks.rslist.api;
 
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,12 +18,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class RsControllerTest {
 
     @Autowired
     MockMvc mockMvc;
 
     @Test
+    @Order(1)
     public void should_get_one_rs_event() throws Exception {
         mockMvc.perform(get("/rs/1"))
                 .andExpect(jsonPath("$.eventName", is("第一条事件")))
@@ -28,6 +33,7 @@ public class RsControllerTest {
     }
 
     @Test
+    @Order(2)
     public void should_get_rs_event_between_start_and_end() throws Exception {
         mockMvc.perform(get("/rs/list?start=1&end=2"))
                 .andExpect(jsonPath("$[0].eventName", is("第一条事件")))
@@ -38,6 +44,7 @@ public class RsControllerTest {
     }
 
     @Test
+    @Order(3)
     public void should_add_rs_event() throws Exception {
         String jsonString = "{\"eventName\":\"猪肉涨价了\", \"keyWord\":\"经济\"}";
         mockMvc.perform(post("/rs/event").content(jsonString).contentType(MediaType.APPLICATION_JSON))
@@ -56,6 +63,7 @@ public class RsControllerTest {
     }
 
     @Test
+    @Order(4)
     public void should_modify_rs_event_when_provide_event_name() throws Exception {
         String jsonString = "{\"eventName\":\"学校放假了\"}";
         mockMvc.perform(post("/rs/event/4").content(jsonString).contentType(MediaType.APPLICATION_JSON))
@@ -74,6 +82,7 @@ public class RsControllerTest {
     }
 
     @Test
+    @Order(5)
     public void should_modify_rs_event_when_provide_key_word() throws Exception {
         String jsonString = "{\"keyWord\":\"政策\"}";
         mockMvc.perform(post("/rs/event/4").content(jsonString).contentType(MediaType.APPLICATION_JSON))
@@ -88,6 +97,25 @@ public class RsControllerTest {
                 .andExpect(jsonPath("$[2].keyWord", is("无标签")))
                 .andExpect(jsonPath("$[3].eventName", is("学校放假了")))
                 .andExpect(jsonPath("$[3].keyWord", is("政策")))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @Order(6)
+    public void should_modify_rs_event_when_provide_key_word_and_event_name() throws Exception {
+        String jsonString = "{\"eventName\":\"晚餐\", \"keyWord\":\"猪蹄\"}";
+        mockMvc.perform(post("/rs/event/4").content(jsonString).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+        mockMvc.perform(get("/rs/list"))
+                .andExpect(jsonPath("$", hasSize(4)))
+                .andExpect(jsonPath("$[0].eventName", is("第一条事件")))
+                .andExpect(jsonPath("$[0].keyWord", is("无标签")))
+                .andExpect(jsonPath("$[1].eventName", is("第二条事件")))
+                .andExpect(jsonPath("$[1].keyWord", is("无标签")))
+                .andExpect(jsonPath("$[2].eventName", is("第三条事件")))
+                .andExpect(jsonPath("$[2].keyWord", is("无标签")))
+                .andExpect(jsonPath("$[3].eventName", is("晚餐")))
+                .andExpect(jsonPath("$[3].keyWord", is("猪蹄")))
                 .andExpect(status().isOk());
     }
 }
