599d2761bbb73891279920dcb4cde434a86848bb
lizeyang1995 <424011527@qq.com>
2020-09-19 00:49:25 +0800

    [lizeyang] feat: write a test, should get vote record when given start time

--- src/test/java/com/thoughtworks/rslist/api/VoteControllerTest.java
+++ src/test/java/com/thoughtworks/rslist/api/VoteControllerTest.java
@@ -2,20 +2,31 @@ package com.thoughtworks.rslist.api;
 
 import com.thoughtworks.rslist.po.RsEventPO;
 import com.thoughtworks.rslist.po.UserPO;
+import com.thoughtworks.rslist.po.VotePO;
 import com.thoughtworks.rslist.repository.RsEventRepository;
 import com.thoughtworks.rslist.repository.UserRepository;
 import com.thoughtworks.rslist.repository.VoteRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.hamcrest.Matchers.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
 @SpringBootTest
 @AutoConfigureMockMvc
 public class VoteControllerTest {
+    @Autowired
+    MockMvc mockMvc;
     @Autowired
     UserRepository userRepository;
     @Autowired
@@ -24,6 +35,7 @@ public class VoteControllerTest {
     VoteRepository voteRepository;
     List<UserPO> userPOS = new ArrayList<>();
     List<RsEventPO> rsEventPOS = new ArrayList<>();
+    List<VotePO> votePOS = new ArrayList<>();
     @BeforeEach
     public void setUp() {
         rsEventRepository.deleteAll();
@@ -38,5 +50,25 @@ public class VoteControllerTest {
         rsEventPOS.add(RsEventPO.builder().eventName("第二条事件").keyWord("无标签").userPO(userPOS.get(0)).voteCount(0).build());
         rsEventPOS.add(RsEventPO.builder().eventName("第三条事件").keyWord("无标签").userPO(userPOS.get(0)).voteCount(0).build());
         rsEventPOS.forEach(item -> rsEventRepository.save(item));
+
+        votePOS.add(VotePO.builder().userPO(userPOS.get(0)).rsEventPO(rsEventPOS.get(0)).voteNum(1).localDate("2020-1-1").build());
+        votePOS.add(VotePO.builder().userPO(userPOS.get(0)).rsEventPO(rsEventPOS.get(1)).voteNum(1).localDate("2020-1-2").build());
+        votePOS.add(VotePO.builder().userPO(userPOS.get(0)).rsEventPO(rsEventPOS.get(2)).voteNum(1).localDate("2020-1-3").build());
+        votePOS.forEach(item -> voteRepository.save(item));
+    }
+
+    @Test
+    void should_get_vote_record_when_given_start_time() throws Exception {
+        mockMvc.perform(get("/voteRecord").param("startTime", "2020-1-1"))
+                .andExpect(jsonPath("$", hasSize(3)))
+                .andExpect(jsonPath("$[0].userId", is(userPOS.get(0).getId())))
+                .andExpect(jsonPath("$[0].rsEventId", is(rsEventPOS.get(0).getId())))
+                .andExpect(jsonPath("$[0].voteNum", is(votePOS.get(0).getVoteNum())))
+                .andExpect(jsonPath("$[1].userId", is(userPOS.get(0).getId())))
+                .andExpect(jsonPath("$[1].rsEventId", is(rsEventPOS.get(1).getId())))
+                .andExpect(jsonPath("$[1].voteNum", is(votePOS.get(1).getVoteNum())))
+                .andExpect(jsonPath("$[2].userId", is(userPOS.get(0).getId())))
+                .andExpect(jsonPath("$[2].rsEventId", is(rsEventPOS.get(2).getId())))
+                .andExpect(jsonPath("$[2].voteNum", is(votePOS.get(2).getVoteNum())));
     }
 }
