03b443e328c98e4c266aedbbb76a07b61a76eede
lizeyang1995 <424011527@qq.com>
2020-09-20 18:13:27 +0800

    [lizeyang] refactor: replace AutoWired with constructor in UserController

--- src/main/java/com/thoughtworks/rslist/api/UserController.java
+++ src/main/java/com/thoughtworks/rslist/api/UserController.java
@@ -1,40 +1,33 @@
 package com.thoughtworks.rslist.api;
 
-import com.fasterxml.jackson.annotation.JsonView;
-import com.thoughtworks.rslist.domain.RsEvent;
 import com.thoughtworks.rslist.domain.User;
 import com.thoughtworks.rslist.po.UserPO;
 import com.thoughtworks.rslist.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.thoughtworks.rslist.service.RsService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.thoughtworks.rslist.exception.Error;
-import com.thoughtworks.rslist.exception.RequestParamNotValid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @RestController
 public class UserController {
-    List<User> userList = initUserList();
     Logger logger = LoggerFactory.getLogger(getClass());
-    @Autowired
-    UserRepository userRepository;
+    private final UserRepository userRepository;
+    private final RsService rsService;
 
-
-    private List<User> initUserList() {
-        userList = new ArrayList<>();
-        return userList;
+    public UserController(UserRepository userRepository, RsService rsService) {
+        this.userRepository = userRepository;
+        this.rsService = rsService;
     }
 
     @PostMapping("/user")
